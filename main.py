141ac89c04a068e88a13e393c57c884c1b590773
kranoley <127663416+kranoley@users.noreply.github.com>
2025-12-07 19:43:50 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import webview
+from web import app
+
+
+window = webview.create_window(
+    'Discord Tokens Checker',
+    app,
+    resizable=False,
+    frameless=True,
+    text_select=False,
+    transparent=True,
+    easy_drag=True,
+    width=850,
+    height=630
+)
+
+webview.start(gui="qt")
+
