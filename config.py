1b2ea1766e5c36df60d3b24cc46efc88c15a9d89
kranoley <127663416+kranoley@users.noreply.github.com>
2025-12-07 19:44:08 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    'max_threads': 100,
+    'max_timeout': 60,
+    'min_threads': 1,
+    'min_timeout': 1,
+    'timeout': 5,
+    'threads': 1,
+    'format': "{token}:{username}:{id}",
+    'all_formats': [
+        'token', 'id', 'username', 'global_name', 'mfa_enabled',
+        'locale', 'email', 'verified', 'phone'
+    ],
+    'tokens': [],
+    'path': ''
+}
